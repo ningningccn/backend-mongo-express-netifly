7fa972fc1a2826e9d6ea0344c8b06b35f14ea3b9
Ning <ning@digisalad.cool>
2023-06-16 11:53:44 +0800

    update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,31 @@
+require("dotenv").config();
 const express = require("express");
 const serverless = require("serverless-http");
 
+// mongodb
+const mongoose = require("mongoose");
+const mongoString = process.env.DATABASE_URL;
+
+// routes
+const routes = require("./routes/routes");
+const todoRouter = require("./routes/todo");
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
 const app = express();
+app.use(express.json());
+app.use("/api", routes);
+app.use("/todo", todoRouter);
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
